eb85ad6680f9290113c70e2b7e512db7c796db39
Your Name <you@example.com>
2022-10-24 17:19:26 -0300

    Aula Stack Navigator

--- App.js
+++ App.js
@@ -2,23 +2,24 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Button, StyleSheet, Text, View } from "react-native";
 
+/**
+ * Cria o componente Home Screen
+ */
 function HomeScreen({ navigation }) {
-  
-  const irParaSobre = () => {
-    return navigation.navigate("Sobre");
-  };
-
   return (
     <View style={styles.container}>
       <Text>Olá Mundo Feliz</Text>
-      <Button 
-        title="Ir para Sobre" 
-        onPress={()=>navigation.navigate("Sobre")} 
+      <Button
+        title="Ir para Sobre"
+        // Realiza a ação de enviar para tela Sobre
+        onPress={() => navigation.navigate("Sobre")}
       />
     </View>
   );
 }
-
+/**
+ *  Criar o componente Sobre Screen
+ */
 function SobreScreen() {
   return (
     <View>
@@ -27,18 +28,25 @@ function SobreScreen() {
   );
 }
 
+// Inicia o Navigator Stack
 const Stack = createNativeStackNavigator();
 
+// Função principal do app
 export default function App() {
   return (
+    // Inicia o container de navegação
     <NavigationContainer>
+      {/*  usa o container de navegação do Stack */}
       <Stack.Navigator>
+        {/* Começa a distribuir as telas usando o Stack.Screen */}
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Sobre" component={SobreScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+// Cria os styles para serem utilizados
+// posteriormente utilizaremos Tailwind
 const styles = StyleSheet.create({
   container: {
     flex: 1,
